af259fb11a5d42cb641aa17d4f3aea1860ae4b20
co323co <co323co@gmail.com>
2021-10-31 07:53:37 +0900

    [FE] Add gitIgnore

--- frontend/src/api/series.js
+++ frontend/src/api/series.js
@@ -2,6 +2,7 @@ import axios from '@/utils/axios';
 
 // 모든 시리즈 목록 조회 API
 function getSeriesList() {
+  console.log(process.env.VUE_APP_API_URL + '/api/series');
   return axios.get('/series');
 }
 
